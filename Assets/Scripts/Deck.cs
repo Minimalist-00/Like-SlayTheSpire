caed943475388a11be997c216f3e0353f2116bbf
Minimalist30 <minimalist0030@gmail.com>
2026-01-01 18:46:03 +0900

    feat: Implement initial card spawning and drawing logic in Deck and create agent system prompt.

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
+    [SerializeField] CardObj cardObjPrefab;
+    [SerializeField] Hand hand;
+    List<CardObj> cardList = new List<CardObj>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            CardObj cardObj = Spawn(); // cardObjインスタンスを生成
+            cardObj.gameObject.SetActive(false);
+            cardList.Add(cardObj);
+        }
     }
 
-    // Update is called once per frame
+    int n = 0;
     void Update()
     {
-        
+        if(n < 4){
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CardObj drawCard = DrawCard();
+                drawCard.transform.SetParent(hand.transform);
+                drawCard.gameObject.SetActive(true);
+                n++;
+            }
+        }
+    }
+
+    private CardObj DrawCard()
+    {
+        CardObj cardObj = cardList[0];
+        cardList.RemoveAt(0);
+        return cardObj;
+    }
+
+    CardObj Spawn()
+    {
+        return Instantiate(cardObjPrefab, transform); // transformはDeckオブジェクトのこと
     }
 }
