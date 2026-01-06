03d6a2fc48767a0420fdbb706193ff48e159d0f8
Minimalist30 <minimalist0030@gmail.com>
2026-01-06 22:14:51 +0900

    Hand: カードの位置が中央揃えで配置される実装

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -11,5 +11,21 @@ public class Hand : MonoBehaviour
     // 親をHandにする
     cardObj.transform.SetParent(transform);
     cardObj.gameObject.SetActive(true);
+    ArrangeCards();
+  }
+
+  // 手札に加えられたカードを整列させる
+  public void ArrangeCards()
+  {
+    for (int i = 0; i < cardList.Count; i++)
+    {
+      // 中央を0にする
+      float center = (cardList.Count - 1) / 2f;
+      // カードの間隔
+      float gap = 100f;
+      // カードの位置
+      float x = (i - center) * gap;
+      cardList[i].transform.localPosition = new Vector3(x, 0, 0); //localPositionは親の位置が基準
+    }
   }
 }
